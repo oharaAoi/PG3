1703a145796b6fa9929711f32efd88d81786b96a
oharaAoi <k023g0108@g.neec.ac.jp>
2024-12-16 10:21:30 +0900

    06_02_確認課題

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 ﻿#include <stdio.h>
+#include <iostream>
+#include <thread>
+
+void ShowThreadId(int num) {
+	std::cout << "thread " << num << std::endl;
+}
 
 int main() {
-	printf("hollo\n");
-	printf("こんにちわ");
+
+	std::thread thread1(ShowThreadId, 1);
+	thread1.join();
+	std::thread thread2(ShowThreadId, 2);
+	thread2.join();
+	std::thread thread3(ShowThreadId, 3);
+	thread3.join();
+	
 	return 0;
 }
