4b80f8482f83800136d6f8bd6ebb9a1e58b04e7f
oharaAoi <k023g0108@g.neec.ac.jp>
2025-01-20 10:36:13 +0900

    07_01_確認課題

--- main.cpp
+++ main.cpp
@@ -1,7 +1,47 @@
-﻿#include <stdio.h>
+﻿#include <cstdio>
+#include <string>
+#include <chrono>
+
+namespace chrono = std::chrono;
 
 int main() {
-	printf("hollo\n");
-	printf("こんにちわ");
+	
+	std::string original(100000, 'a');
+
+	// ------------------------------------------------------------------ //
+	// コピーを行う
+	// ------------------------------------------------------------------ //
+	std::chrono::system_clock::time_point copyStart, copyEnd;
+	copyStart = std::chrono::system_clock::now();
+
+	// コピー
+	std::string copy = original;
+	copyEnd = std::chrono::system_clock::now();
+
+	// 時間を計算
+	float copyElapsed = chrono::duration<float, std::micro>(copyEnd - copyStart).count();
+
+
+	// ------------------------------------------------------------------ //
+	// 移動を行う
+	// ------------------------------------------------------------------ //
+	std::chrono::system_clock::time_point moveStart, moveEnd;
+	moveStart = std::chrono::system_clock::now();
+
+	// 移動
+	std::string move = std::move(original);
+	moveEnd = std::chrono::system_clock::now();
+
+	// 時間を計算
+	float moveElapsed = chrono::duration<float, std::micro>(moveEnd - moveStart).count();
+
+	// ------------------------------------------------------------------ //
+	// 表示をする
+	// ------------------------------------------------------------------ //
+
+	printf("100,00文字を移動とコピーで比較しました\n");
+	printf("コピー: %f\n", copyElapsed);
+	printf("移動 : %f\n", moveElapsed);
+
 	return 0;
 }
